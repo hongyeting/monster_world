29ba93a2e205bcc9418c0a671d8ab017ee359b05
hongyt <77911615+hongyeting@users.noreply.github.com>
2021-02-17 14:24:43 +0800

    Update check_helper.cpp

--- check_helper.cpp
+++ check_helper.cpp
@@ -8,7 +8,7 @@ ifstream cin1("D:\\VS\\Warcraft.out");//标答
 ifstream cin2("D:\\VS\\cindata.txt");//WA
 ofstream cout0("D:\\VS\\coutdata.txt");
 
-string s[2];
+string s[3];
 
 int main()
 {
@@ -19,9 +19,12 @@ int main()
 		x++;
 		getline(cin1, s[0]);
 		getline(cin2, s[1]);
+		if (s[0][0] == 'C')
+			s[2] = s[0];
 		if (s[0] != s[1])
 		{
 			w++;
+			if (w == 1)cout << s[2] << '\n' << '\n';
 			cout0 << s[0] << '\n' << s[1] << '\n' << "wrong " << w << '\n' << '\n';
 		}
 	}
