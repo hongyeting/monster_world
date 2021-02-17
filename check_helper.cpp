37983c8e6a2ff879c3e77f9b1cb1769d36d2d308
hongyt <77911615+hongyeting@users.noreply.github.com>
2021-02-17 14:13:33 +0800

    Update check_helper.cpp

--- check_helper.cpp
+++ check_helper.cpp
@@ -1,29 +1,30 @@
+#include<iostream>
 #include<fstream>
 #include<string>
 
 using namespace std;
 
-ifstream cin("D:\\VS\\cindata.txt");
-ofstream cout("D:\\VS\\coutdata.txt");
+ifstream cin1("D:\\VS\\Warcraft.out");//标答
+ifstream cin2("D:\\VS\\cindata.txt");//WA
+ofstream cout0("D:\\VS\\coutdata.txt");
 
-string s[10000];
+string s[2];
 
 int main()
 {
-	int x = 0;//x lines
-	for (;;x++)
+	int x = 0;
+	int w = 0;
+	while (cin1.peek() != '\n' && cin2.peek() != '\n' && w < 50)
 	{
-		getline(cin, s[x]);cout << 1;
-		if (s[x] == "abcdefg" || x == 9999)break;
+		x++;
+		getline(cin1, s[0]);
+		getline(cin2, s[1]);
+		if (s[0] != s[1])
+		{
+			w++;
+			cout0 << s[0] << '\n' << s[1] << '\n' << "wrong " << w << '\n' << '\n';
+		}
 	}
-	string ss;
-	int wrong = 0;
-	for (int i = 0;i < x;i++)
-	{
-		getline(cin, ss);
-		if (ss == s[i])cout << "right" << i + 1 << '\n';
-		else { wrong++;cout << s[i] << '\n' << '\n' << "wrong" << i + 1 << " " << wrong << '\n'; }
-	}
-	cout << wrong;
+	cout << w << '\n';
 	return 0;
 }
