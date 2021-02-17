fdba7dd59eb01a6675a9c78e53b5db1c68941ccd
hongyt <77911615+hongyeting@users.noreply.github.com>
2021-02-17 19:16:08 +0800

    Update check_helper.cpp

--- check_helper.cpp
+++ check_helper.cpp
@@ -8,26 +8,39 @@ ifstream cin1("D:\\VS\\Warcraft.out");//标答
 ifstream cin2("D:\\VS\\cindata.txt");//WA
 ofstream cout0("D:\\VS\\coutdata.txt");
 
-string s[3];
+int id[2] = { 0 };
+string s[2][10000000];
 
 int main()
 {
-	int x = 0;
-	int w = 0;
-	while (cin1.peek() != '\n' && cin2.peek() != '\n' && w < 50)
-	{
-		x++;
-		getline(cin1, s[0]);
-		getline(cin2, s[1]);
-		if (s[0][0] == 'C')
-			s[2] = s[0];
-		if (s[0] != s[1])
-		{
-			w++;
-			if (w == 1)cout << s[2] << '\n' << '\n';
-			cout0 << s[0] << '\n' << s[1] << '\n' << "wrong " << w << '\n' << '\n';
+	for (int i = 0;i < 27;i++) {
+		cout << i + 1;
+		id[0] = id[1] = 0;
+		getline(cin1, s[0][0]);
+		getline(cin2, s[1][0]);
+		while (cin1.peek() != '\n' && cin1.peek() != 'C')
+			getline(cin1, s[0][++id[0]]);
+		bool wrong = false;
+		while (cin2.peek() != '\n' && cin2.peek() != 'C') {
+			getline(cin2, s[1][++id[1]]);
+			if (!wrong && (id[1] > id[0] || s[1][id[1]] != s[0][id[1]]))
+				wrong = true;
 		}
+		if (id[1] < id[0])wrong = true;
+		if (wrong) {
+			int wrong = 0, j = 0;
+			for (;j < id[0] && j < id[1];j++) {
+				if (s[0][j] == s[1][j])cout0 << s[0][j] << '\n';
+				else cout0 << '\n' << "wrong " << ++wrong << '\n' << s[0][j] << '\n' << s[1][j] << '\n';
+			}
+			for (;j < id[0];j++)
+				cout0 << "default " << ++wrong << '\n' << s[0][j] << '\n';
+			for (;j < id[1];j++)
+				cout0 << "surplus" << ++wrong << '\n' << s[1][j] << '\n';
+			cout0 << '\n' << '\n';
+			cout << "wrong" << '\n';
+		}
+		cout << "correct" << '\n';
 	}
-	cout << w << '\n';
 	return 0;
 }
